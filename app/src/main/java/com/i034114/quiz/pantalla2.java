d02496db359cb5ec6d6f3d32dc162219d361c23d
ivandiaz2425 <ivandiaz2422@gmail.com>
2017-08-24 14:00:09 -0500

    sexto

--- app/src/main/java/com/i034114/quiz/pantalla2.java
+++ app/src/main/java/com/i034114/quiz/pantalla2.java
@@ -1,7 +1,10 @@
 package com.i034114.quiz;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class pantalla2 extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class pantalla2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goActivityUno(view);
+
+            }
+        });
+
+
+    }
+
+    public void goActivityUno(View view){
+        Intent intent = new Intent(this, pantalla1.class);
+        startActivity(intent);
+    }
+
+
+
+
+}
     }
 }
