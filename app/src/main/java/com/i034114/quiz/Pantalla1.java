ef13d42070ab10e21f0434b6e7e31ccd156223a1
ivandiaz2425 <ivandiaz2422@gmail.com>
2017-08-24 13:55:48 -0500

    quinto

--- app/src/main/java/com/i034114/quiz/Pantalla1.java
+++ app/src/main/java/com/i034114/quiz/Pantalla1.java
@@ -1,7 +1,11 @@
 package com.i034114.quiz;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Pantalla1 extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class Pantalla1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla1);
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goActivityDos(view);
+
+            }
+        });
+
+
     }
-}
+
+    public void goActivityDos(View view){
+        Intent intent = new Intent(this, pantalla2.class);
+        startActivity(intent);
+    }
+
+
+
+
+    }
+
